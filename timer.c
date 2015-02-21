ce6dd51bc81aeccb758f471ba1226eb3f96e2b6c
ruelle <ruelle@ruelle-System-Product-Name.(none)>
2015-02-21 13:23:26 +0100

    Improvements in timer.c

--- timer.c
+++ timer.c
@@ -1,20 +1,15 @@
-#include <stddef.h>
-#include <sys/time.h>
+#include <time.h>
 
 #include "timer.h"
 
 int startTimer( int timeout )
 {
-	struct timeval tv;
-	gettimeofday( &tv, NULL );
-	return ( (int) tv.tv_sec + timeout ) * 256 + (int) tv.tv_usec / 4096;
+	struct timespec tp;
+	clock_gettime( CLOCK_MONOTONIC, &tp );
+	return ( (int) tp.tv_sec + timeout ) * 512 + (int) tp.tv_nsec / 2097152;
 }
 
 int checkTimer( int t )
 {
-	struct timeval tv;
-	gettimeofday( &tv, NULL );
-	return t - ( (int) tv.tv_sec * 256 + (int) tv.tv_usec / 4096 ) <= 0;
+	return t - startTimer( 0 ) <= 0;
 }
-
-// clock_gettime( CLOCK_MONOTONIC wäre vielleicht besser
